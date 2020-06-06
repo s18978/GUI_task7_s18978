cf1cfda098c9b8a1cc2f3adcd40def2a58576049
s18978 <61871821+s18978@users.noreply.github.com>
2020-06-06 12:25:25 +0200

    tut11

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+        // public string Welcome(string name, int numTimes = 1)
+        // {
+        //     return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        // }
+    }
+}
